f6c4bc7c047046ea177b1adab59620c9b13bcf43
opkke <38033026+opkke@users.noreply.github.com>
2018-07-11 16:19:03 +0800

    Update pwm-zynq.c

--- pwm-zynq.c
+++ pwm-zynq.c
@@ -2,6 +2,10 @@
 #include <linux/ioport.h>
 #include <linux/pwm.h>
 #include <linux/device.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/clk.h>
+#include <linux/err.h>
 
 struct zynq_pwm_chip {
     // gpio has no device?
@@ -10,6 +14,14 @@ struct zynq_pwm_chip {
     void __iomem *base_addr;
     struct clk *clk;
 };
+
+static int zynq_pwm_config(struct pwm_chip *chip, struct pwm_device *pwm, int duty_ns, int period_ns);
+static int zynq_pwm_set_polarity(struct pwm_chip *chip, struct pwm_device *pwm, enum pwm_polarity polarity);
+static int zynq_pwm_enable(struct pwm_chip *chip, struct pwm_device *pwm);
+static void zynq_pwm_disable(struct pwm_chip *chip, struct pwm_device *pwm);
+static int zynq_pwm_request(struct pwm_chip *chip, struct pwm_device *pwm);
+static void zynq_pwm_free(struct pwm_chip *chip, struct pwm_device *pwm);
+static int zynq_pwm_remove(struct platform_device *pdev);
 
 static const struct pwm_ops zynq_pwm_ops = {
     .config = zynq_pwm_config,
@@ -33,21 +45,16 @@ static const struct of_device_id zynq_pwm_of_match[] = {
 MODULE_DEVICE_TABLE(of, zynq_pwm_of_match);
 
 
-static int zynq_pwm_config(struct pwm_chip *chip, struct pwm_device *pwm, int duty_ns, int period_ns);
-static int zynq_pwm_set_polarity(struct pwm_chip *chip, struct pwm_device *pwm, enum pwm_polarity polarity);
-static int zynq_pwm_enable(struct pwm_chip *chip, struct pwm_device *pwm);
-static void zynq_pwm_disable(struct pwm_chip *chip, struct pwm_device *pwm);
-static int zynq_pwm_request(struct pwm_chip *chip, struct pwm_device *pwm);
-static void zynq_pwm_free(struct pwm_chip *chip, struct pwm_device *pwm);
-static int zynq_pwm_remove(struct platform_device *pdev);
+
 
 
 static int zynq_pwm_probe(struct platform_device *pdev) {
     struct zynq_pwm_chip *zynq_pwm;
     struct resource *res;
     struct pwm_device *pwm;
+    int ret,i;
     // allocate memory
-    pwm = devm_kzalloc(&pdev->dev, sizeof(*pwm), GFP_KERNEL)
+    pwm = devm_kzalloc(&pdev->dev, sizeof(*pwm), GFP_KERNEL);
     if (!pwm)
         return -ENOMEM;
     // get the base addr to res
@@ -58,21 +65,24 @@ static int zynq_pwm_probe(struct platform_device *pdev) {
     if (IS_ERR(zynq_pwm->base_addr))
         return PTR_ERR(zynq_pwm->base_addr);
     // get the clock
-    zynq_pwm->clk = devm_clk_get($pdev->dev, NULL);
-
+    zynq_pwm->clk = devm_clk_get(&pdev->dev, NULL);
+        if (IS_ERR(zynq_pwm->clk)) {
+	    dev_err(&pdev->dev, "failed to get pwm clock\n");
+	    return PTR_ERR(zynq_pwm->clk);
+    }
     // pwm_chip settings binding
     zynq_pwm->chip.dev = &pdev->dev;
     zynq_pwm->chip.ops = &zynq_pwm_ops;
     zynq_pwm->chip.base = -1;
     zynq_pwm->chip.npwm = 1;
-    zynq_pwm->chip.of_xlate = of_pwm_xlate_with_flags;
+    //zynq_pwm->chip.of_xlate = of_pwm_xlate_with_flags;
     zynq_pwm->chip.of_pwm_n_cells = 1;
 
     ret = pwmchip_add(&zynq_pwm->chip);
     if (ret < 0) {
         dev_err(&pdev->dev, "pwmchip_add failed: %d\n", ret);
         goto disable_pwmclk;
-    }   
+     }   
     // add each pwm device
     for (i = 0; i < zynq_pwm->chip.npwm; i++) {
             struct zynq_pwm_data *data;
@@ -81,19 +91,28 @@ static int zynq_pwm_probe(struct platform_device *pdev) {
             data = devm_kzalloc(zynq_pwm->dev, sizeof(*data), GFP_KERNEL);
             if (!data) {
                 ret = -ENOMEM;
-                goto remove_pwmchip;
+                goto pwmchip_remove;
             }
 
             pwm_set_chip_data(pwm, data);
     }
 
     // after finish all task below, set driver data to platform.
-    platform_set_drvdata(pdev, pwm)
+    platform_set_drvdata(pdev, pwm);
+    return 0;
 
-    remove_pwmchip:
+    pwmchip_remove:
     pwmchip_remove(&zynq_pwm->chip);
+    disable_pwmclk:
+    clk_disable_unprepare(zynq_pwm->clk);
     return ret;
 
+}
+
+static int zynq_pwm_remove(struct platform_device *pdev) {
+    struct zynq_pwm_chip *zynq_pwm = platform_get_drvdata(pdev);
+    clk_disable_unprepare(zynq_pwm->clk);
+    return pwmchip_remove(&zynq_pwm->chip);
 }
 
 static struct platform_driver zynq_pwm_driver = {
@@ -104,3 +123,17 @@ static struct platform_driver zynq_pwm_driver = {
     .probe = zynq_pwm_probe,
     .remove = zynq_pwm_remove,
 };
+
+static int __init zynq_pwm_init(void)
+{
+	return platform_driver_register(&zynq_pwm_driver);
+}
+subsys_initcall(zynq_pwm_init);
+
+static void __exit zynq_pwm_exit(void)
+{
+	platform_driver_unregister(&zynq_pwm_driver);
+}
+module_exit(zynq_pwm_exit);
+
+MODULE_LICENSE("GPL"); 
